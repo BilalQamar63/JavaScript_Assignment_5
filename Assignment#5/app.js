d496571404d2be2ec0338515ab76a637a0379d06
BilalQamar63 <143898156+BilalQamar63@users.noreply.github.com>
2023-10-02 11:23:13 +0500

    Update app.js

--- Assignment#5/app.js
+++ Assignment#5/app.js
@@ -47,23 +47,263 @@ console.log(userInput);*/
 
 // Chapter 22-25 ******* (Question 2)********
 
-var sameWords = "captain";
-var numberOfCharacters = sameWords.length;
-var slicedPortion = sameWords.slice(1, numberOfCharacters - 1);
-console.log(slicedPortion); 
+// var sameWords = "captain";
+// var numberOfCharacters = sameWords.length;
+// var slicedPortion = sameWords.slice(1, numberOfCharacters - 1);
+// console.log(slicedPortion); 
 
 // Chapter 22-25 ******* (Question 3)********
+// var animal = "elephant";
+// var seg = animal.slice(1, 5);
+// console.log(seg);
+
 // Chapter 22-25 ******* (Question 4)********
+
+// var myString = "Hello World"
+// var stringLength = myString.length;
+// console.log(stringLength)
+
 // Chapter 22-25 ******* (Question 5)********
+
+// var myStringstring = "Hello Universe"
+// var stringLength = myString.length;
+// var slicedString = myString.slice(1, -3);
+// console.log("orignal length ",myString);
+// console.log("sliced string ",slicedString)
+
 // Chapter 22-25 ******* (Question 6)********
+
+// var text = "To be or not to be.";
+// var indx = text.indexOf("be");
+// console.log(indx);
+
 // Chapter 22-25 ******* (Question 7)********
+
+// var text = "To be or not to be.";
+// var indx = text.lastIndexOf("be");
+// console.log(indx)
+
 // Chapter 22-25 ******* (Question 8)********
+
+// var text = "Now we go to the park";
+// var lastIndexOfGo = text.lastIndexOf("go");
+// var indx = lastIndexOfGo;
+
+// console.log(indx);
+
 // Chapter 22-25 ******* (Question 9)********
+
+    // Sir ya question samaj nahi ayaa
+
 // Chapter 22-25 ******* (Question 10)********
+
+// var str = "abcde";
+// var characterAtIndex2 = str.charAt(2);
+// console.log(characterAtIndex2); 
+
 // Chapter 22-25 ******* (Question 11)********
+
+// var text2 = "This is a sample text";
+// var cha = text2.charAt(9);
+// console.log(cha);
+
+
 // Chapter 22-25 ******* (Question 12)********
+
+// var str = "This is a sample string.";
+// var x = str.charAt(str.length - 1);
+// console.log(x);
+
 // Chapter 22-25 ******* (Question 13)********
+
+// var input = "Hello, World!"
+// var cha = input.charAt(4)
+// console.log(input)
+
 // Chapter 22-25 ******* (Question 14)********
+
+// var myString = "example";
+// var targetCharacter = "a";
+
+// if (myString.charAt(2) === targetCharacter) {
+//    console.log("Index 2 is a")
+// } else {
+//    console.log("Index 2 is not a")
+// }
+
 // Chapter 22-25 ******* (Question 15)********
+
+// Sir ya samaj nahi ayaa?
+
 // Chapter 22-25 ******* (Question 16)********
+
+// var reply = "No, I have not seen it. No , I don't want to.";
+// var revisedReply = reply.replace("No", "Yes");
+
+// console.log(revisedReply); 
+
 // Chapter 22-25 ******* (Question 17)********
+
+// var str = "This is 1 example of 1.";
+// var newStr = str.replace("1", "one");
+
+// console.log(newStr);
+
+// Chapter 26 ******* (Question 1)********
+
+// var x = "banana";
+// var y = x.replace(/a/g, "z");
+
+// console.log(y);
+
+// Chapter 26 ******* (Question 2)********
+
+// var numberToRound = 3.7;
+// var roundedNumber = Math.round(numberToRound);
+
+// console.log(roundedNumber); 
+
+// Chapter 26 ******* (Question 3)********
+
+//  var numberToRound = 3.1;
+// var roundedNumber = Math.round(numberToRound);
+// console.log(roundedNumber); 
+
+// Chapter 26 ******* (Question 4)********
+
+// var originalNumber = 5.7;
+// var roundedNumber = Math.round(originalNumber);
+
+// console.log(roundedNumber); 
+
+
+
+// Chapter 26 ******* (Question 5)********
+
+// var originalNumber = .5;
+// var roundedNumber = Math.round(originalNumber);
+
+// console.log(roundedNumber); 
+
+
+
+// Chapter 27 ******* (Question 1)********
+
+// var randomFraction = Math.random();
+// var genratedNumber = Math.floor(randomFraction* 50) + 1
+// console.log(genratedNumber)
+
+
+// Chapter 27 ******* (Question 2)********
+
+// var randomNumber = Math.random()
+// console.log(randomNumber);
+
+// Chapter 27 ******* (Question 3)********
+
+// Generate a random number between 1 and 6
+// var diceRoll = Math.floor(Math.random() * 6) + 1;
+
+// console.log("You rolled a " + diceRoll);
+
+// Chapter 27 ******* (Question 4)********
+
+// var randomOutcome = Math.random();
+// var tossResult = (randomOutcome < 0.5) ? "Heads" : "Tails";
+// console.log("The coin landed on: " + tossResult);
+
+// Chapter 28-29 ******* (Question 1)********
+
+// var str = "567";
+// var num = parseInt(str);
+// console.log(num);
+
+// Chapter 28-29 ******* (Question 2)********
+
+
+// var str = "123";
+// var num = parseInt(str);
+// console.log(num);
+
+
+// Chapter 28-29 ******* (Question 3)********
+
+// var str = "2.718";
+// var num = Number(str);
+
+// console.log(num);
+
+
+// Chapter 28-29 ******* (Question 4)********
+
+// Samaj nahi ayaa Sir
+
+// Chapter 28-29 ******* (Question 5)********
+
+// var number = 42;
+// var str = number.toString();
+
+// console.log(str);
+
+// Chapter 28-29 ******* (Question 6)********
+
+// function numberToString() {
+//     var number = 42;
+//     var str = number.toString();
+//     return str;
+//   }
+  
+//   var result = numberToString();
+//   console.log(result);
+  
+// Chapter 28-29 ******* (Question 7)********
+
+// var decimalString = "3.14";
+// var floatValue = parseFloat(decimalString);
+// var intValue = Math.round(floatValue);
+
+// console.log(intValue); 
+
+// Chapter 30 ******* (Question 1)********
+
+// var num = 3.14159; 
+// var newNum = num.toFixed(4);
+
+// console.log(newNum); 
+
+// Chapter 30 ******* (Question 2)********
+
+// var num = 3.14159; 
+// var newNum = num.toFixed(2);
+
+// console.log(newNum); 
+
+// Chapter 30 ******* (Question 3)********
+
+// var num = 123.4567;
+// var numAsString = num.toFixed(2).toString();
+
+// if (numAsString.length > 4) {
+//    console.log(numAsString)
+// }
+
+
+// Chapter 30 ******* (Question 4)********
+
+// var originalNumber = 3.14159265359; 
+// var roundedString = originalNumber.toFixed(2);
+// alert("Rounded to 2 decimal places: " + roundedString);
+
+
+/* Palindrome Word */
+
+// var string3 = prompt("Please enter a Any word")
+// var len = string3.length;
+// var msg = "It is a Palindrome";
+// for(var i=0; i < len/2; i++){
+//     if (string3[i] != string3[len - 1 -i]){
+//         msg = "It is not a Palindrome"
+//     }
+// }
+
+// console.log(`${string3}: ${msg}`)
